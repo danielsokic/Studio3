d14b3427f37fb90d185b8fc61c4f28ca2fc1f795
Daniel Sokic <44309771+danielsokic@users.noreply.github.com>
2025-02-26 18:00:47 -0800

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    float moveSpeed = 5f;
+    float jumpForce = 7f;
+    float gravity = 9.81f;
+    private CharacterController controller;
+    private Vector3 moveDirection;
+    private float yVelocity;
+   public Rigidbody playerBody;
+   public Transform cameraTransform;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        if (cameraTransform == null && Camera.main != null)
+        {
+            cameraTransform = Camera.main.transform;
+        }
+    }
+
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 camForward = cameraTransform.forward;
+        Vector3 camRight = cameraTransform.right;
+
+
+        camForward.y = 0;
+        camRight.y = 0;
+        camForward.Normalize();
+        camRight.Normalize();
+
+
+        Vector3 move = (camForward * moveZ + camRight * moveX).normalized;
+        moveDirection = move * moveSpeed;
+
+        if(controller.isGrounded){
+            yVelocity = 0;
+            if(Input.GetKeyDown(KeyCode.Space)){
+                yVelocity = jumpForce;
+            }
+        }else{
+            yVelocity -= gravity * Time.deltaTime;
+        }
+
+        moveDirection.y = yVelocity;
+        controller.Move(moveDirection * Time.deltaTime);
+
+    }
+}
